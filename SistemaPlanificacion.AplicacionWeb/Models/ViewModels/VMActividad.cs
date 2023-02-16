a343ae2ac52bac3257f1c107994b42ab4144f83d
David Michel Lopez <David Michel Lopez@MICHELSISTEMAS>
2023-02-16 15:32:49 -0400

    Agregar archivos de proyecto.

---
+++ SistemaPlanificacion.AplicacionWeb/Models/ViewModels/VMActividad.cs
@@ -0,0 +1,10 @@
+﻿using SistemaPlanificacion.Entity;
+
+namespace SistemaPlanificacion.AplicacionWeb.Models.ViewModels
+{
+    public class VMActividad
+    {
+        public string IdActividad { get; set; } = null!;
+        public string? NombreActividad { get; set; }
+    }
+}
---
+++ SistemaPlanificacion.AplicacionWeb/Models/ViewModels/VMCentroSalud.cs
@@ -0,0 +1,10 @@
+﻿using SistemaPlanificacion.Entity;
+
+namespace SistemaPlanificacion.AplicacionWeb.Models.ViewModels
+{
+    public class VMCentroSalud
+    {
+        public string IdCentro { get; set; } = null!;
+        public string? NombreCentro { get; set; }
+    }
+}
